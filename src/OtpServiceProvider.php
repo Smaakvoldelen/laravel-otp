a3d01c79098db7656975b1d4ceb4701f6ac57d0a
Michael Beers <michael@michaelbeers.nl>
2024-05-01 18:43:44 +0200

    Clean up the package and fix test and phpstan workflows

--- src/OtpServiceProvider.php
+++ src/OtpServiceProvider.php
@@ -4,22 +4,14 @@ namespace Smaakvoldelen\Otp;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Smaakvoldelen\Otp\Commands\OtpCommand;
 
 class OtpServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-otp')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-otp_table')
-            ->hasCommand(OtpCommand::class);
+            ->hasMigration('create_otps_table');
     }
 }
